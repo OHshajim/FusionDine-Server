be33c6fc6b7c1da139b55f6302f3e064de846cb6
OHshajim <ajshajimmax@gmail.com>
2024-05-10 21:11:58 +0600

    file setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
